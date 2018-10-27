a6916032e88d6f9b8dcd8b012559b2fdacdd2e83
Mike North <michael.l.north@gmail.com>
2018-10-27 11:59:40 -0700

    test fixes

--- my-addon/tests/integration/components/x-foo-test.ts
+++ my-addon/tests/integration/components/x-foo-test.ts
@@ -12,7 +12,7 @@ module('Integration | Component | x-foo', function(hooks) {
 
     await render(hbs`{{x-foo}}`);
 
-    assert.equal(this.element.textContent.trim(), '');
+    assert.equal(('' + this.element.textContent).trim(), '');
 
     // Template block usage:
     await render(hbs`
@@ -21,6 +21,6 @@ module('Integration | Component | x-foo', function(hooks) {
       {{/x-foo}}
     `);
 
-    assert.equal(this.element.textContent.trim(), 'template block text');
+    assert.equal(('' + this.element.textContent).trim(), 'template block text');
   });
 });
